18555dddb523eb89f101a5f1b1fe13200f709113
catalin.voineag <catalin.voineag@onthebeach.co.uk>
2021-02-12 23:22:11 +0000

    Remove the comments in the Gemfile, the script almost works as it is
    All the tasks except the db_setup work, this commit removes the comments
    in the gemfile and comments out all the tasks except the db_setup

--- full_rails_template.rb
+++ full_rails_template.rb
@@ -2,29 +2,40 @@ require 'byebug'
 require 'fileutils'
 
 def add_gems
-  add_source 'https://rubygems.org' do
-    gem 'httparty'
-    gem 'id'
+  gem 'httparty'
+  gem 'devise'
+  gem 'id'
 
-    gem_group :development do
-      gem 'foreman'
-    end
-
-    gem_group :development, :test do
-      gem 'rspec-rails'
-      gem 'rubocop'
-      gem 'awesome_print'
-    end
-
-    gem_group :test do
-      gem 'database_cleaner'
-      gem 'factory_bot_rails'
-      gem 'shoulda-matchers'
-      gem 'simplecov'
-      gem 'timecop'
-      gem 'vcr'
-    end
+  gem_group :development do
+    gem 'foreman'
   end
+
+  gem_group :development, :test do
+    gem 'awesome_print'
+    gem 'rspec-rails'
+    gem 'rubocop'
+  end
+
+  gem_group :test do
+    gem 'database_cleaner'
+    gem 'factory_bot_rails'
+    gem 'shoulda-matchers'
+    gem 'simplecov'
+    gem 'timecop'
+    gem 'vcr'
+  end
+end
+
+def remove_comments_from_gemfile
+  File.open('Gemfile_tmp', 'w') do |output_file|
+
+    File.foreach('Gemfile') do |line|
+      output_file.puts line unless line.include?('#') || line == "\n"
+    end
+
+  end
+
+  FileUtils.mv('Gemfile_tmp', 'Gemfile')
 end
 
 def setup_spec
@@ -39,14 +50,14 @@ end
 def git_init
   git :init
   git add: "."
-  #git commit: "-a -m 'Initial commit'"
+  git commit: "-a -m 'Initial commit'"
 end
 
 def remove_add_gitignore_lines
-  File.open('.gitignore_tmp', "w") do |output_file|
+  File.open('.gitignore_tmp', 'w') do |output_file|
 
     File.foreach('.gitignore') do |line|
-      output_file.puts line unless BLACKLISTED_LINES.include?(line) || line.include?('#')
+      output_file.puts line unless BLACKLISTED_LINES.include?(line)
     end
 
     GITIGNORE_LINES.each do |line|
@@ -58,10 +69,9 @@ def remove_add_gitignore_lines
 end
 
 def create_spec_structure
-  #Dir.mkdir 'spec' unless File.exists?('spec')
-  Dir.mkdir 'spec/shared_examples' unless File.exists?('shared_examples')
-  Dir.mkdir 'spec/cassettes' unless File.exists?('cassettes')
-  Dir.mkdir 'spec/stubs' unless File.exists?('stubs')
+  Dir.mkdir 'spec/shared_examples' unless File.exists?('spec/shared_examples')
+  Dir.mkdir 'spec/cassettes' unless File.exists?('spec/cassettes')
+  Dir.mkdir 'spec/stubs' unless File.exists?('spec/stubs')
 end
 
  BLACKLISTED_LINES = [
@@ -87,12 +97,13 @@ end
     ".env.local"
  ]
 
+remove_comments_from_gemfile
 add_gems
 
 after_bundle do
-  #remove_add_gitignore_lines
+  remove_add_gitignore_lines
   #db_setup
   setup_spec
   create_spec_structure
-  #git_init
+  git_init
 end
